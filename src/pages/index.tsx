712327f25b058a4305b2c7ce6ddb2b485aa7b21d
Duy Đạt Phạm <duydatpham@gmail.com>
2022-06-16 21:11:47 +0700

    #dat

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import TreeView from '@/layouts/TreeView';
+import { useEffect, useState } from 'react';
 
 
 const Index = () => {
+  const [show, setShow] = useState(false)
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(true)
+    }, 5000);
+  }, [])
   return (
     <div>
-      {/* <a>sddfsdfsfsdd</a> */}
-      <TreeView />
+      {show && <TreeView />}
     </div>
   );
 };
